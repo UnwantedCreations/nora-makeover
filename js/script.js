2443ea7f2c898d69ed03ca5832b1bee74e30e245
Arun <unwantedarun@hotmail.com>
2024-09-05 16:49:12 +0530

    BOOTSTRAP ICONS

--- js/script.js
+++ js/script.js
@@ -60,49 +60,49 @@
 	// Overlay Menu Navigation
 	// ------------------------------------------------------------------------------ //
 	var overlayMenu = function () {
-
-		if(!$('.nav-overlay').length) {
-		  return false;
-		}
-
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-		  body = document.querySelector('body');
-		  menu = document.querySelector('.menu-btn');
-		  menuItems = document.querySelectorAll('.nav__list-item');
-		  applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-		  menu.addEventListener('click', function () {
-		    return toggleClass(body, 'nav-active');
-		  });
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-		  if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}
-
+    if(!$('.nav-overlay').length) {
+      return false;
+    }
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+      body = document.querySelector('body');
+      menu = document.querySelector('.menu-btn');
+      menuItems = document.querySelectorAll('.nav__list-item');
+      applyListeners();
+    };
+    var applyListeners = function applyListeners() {
+      menu.addEventListener('click', function (e) {
+        e.preventDefault();
+        return toggleClass(body, 'nav-active');
+      });
+      menuItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+          toggleClass(body, 'nav-active');
+          toggleClass(menu, 'menu-btn-active');
+        });
+      });
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+      if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+      else element.classList.add(stringClass);
+    };
+    init();
+  }
+  
   $(document).ready(function(){
     overlayMenu();
     initChocolat();
-    
-    AOS.init({
-      duration: 1000,
-      once: true
-    })
-
+    AOS.init({ duration: 1000, once: true })
     $('#btn-menu').click(function(e){
       e.preventDefault();
       $('.sidebar-menu').toggleClass('open');
     })
-
-  }); // End of a document
-
+  }); // End of document
+  
   $(window).load(function () {
     initIsotope();
   });
-
+  
 })(jQuery);
